826b4c860c30c672b10550735f8ad2219f364bbc
utsavbalar1231 <utsavbalar1231@gmail.com>
2023-01-17 12:20:09 +0000

    PKGBUILDS: Fixes for nginx and AUR
    Change-Id: I7c3dd31f02b4434c980b2b5da8b781d5354c558b
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- update.sh
+++ update.sh
@@ -1,19 +1,41 @@
 #!/usr/bin/env bash
+set -e
+
+path="/usr/share/nginx/html/vaaman"
 
 files=$(find . -name "PKGBUILD")
-for f in $files
-do
-        d=$(dirname "$f")
-        cd "$d"  || exit
-		makepkg -s --noconfirm --skippgpcheck "$@"
-		makepkg --printsrcinfo > /tmp/srcinfo && mv /tmp/srcinfo .SRCINFO
-		sudo rm -rf src pkg "$d"
-        cd ..
+for f in $files; do
+	d=$(dirname "$f")
+
+	# check if the package ends with -git
+	if [[ $d == *-git ]]; then
+		d=${d:2}
+		f=${f:2}
+
+		if [[ ! -d $path/$d ]]; then
+			sudo mkdir -p $path/$d
+		fi
+
+		# copy the PKGBUILD to the path
+		sudo cp -avf "$f" "$path/$d/PKGBUILD"
+
+	else
+		cd "$d" || exit
+		makepkg -s -f --noconfirm --skippgpcheck "$@"
+		makepkg --printsrcinfo >.SRCINFO
+
+		build_files=$(find . -name "*.tar.zst")
+		sudo cp -av $build_files /usr/share/nginx/html/vaaman/
+
+		cd .. || exit
+	fi
 done
 
 # repo-add to vaaman.db
-files=$(find . -name "*.pkg.tar.zst")
-for f in $files
-do
-		sudo repo-add vaaman.db.tar.gz "$f"
+files=$(find . -name "*.tar.zst")
+echo "Updating vaaman.db with $files"
+
+for f in $files; do
+	f=${f:2}
+	sudo repo-add -s -n -R /usr/share/nginx/html/vaaman/vaaman.db.tar.zst "$f"
 done
